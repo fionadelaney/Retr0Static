0642d638c7f58e40e8165b554b6e59d34b2834d5
Fiona Delaney <B00092671@student.itb.ie>
2016-04-07 13:02:09 +0100

    more object-based work on shop listing

--- stage_02_php/src/mainController.php
+++ stage_02_php/src/mainController.php
@@ -54,137 +54,126 @@ function shopListingAction()
 
     $game_list = array();
     $game_list[] = array(
-            'game_id' => 'BIG001_',
-            'game_title' => 'Bioshock',
-            'game_platform' => 'XBox 360 2007',
-            'game_price' => '&euro; 6.00',
-            'game_screen' => 'bioshock_ss.jpg',
-            'game_dev' => 'Irrational Games',
-            'game_dev_url' => 'http://irrationalgames.com/tag/bioshock/',
-            'game_desc' => 'Fantasy 1st person shooter'
-        );
+   //         'game_id' => 'BIG001_',
+   //         'game_title' => 'Bioshock',
+   //         'game_platform' => 'XBox 360 2007',
+   //         'game_price' => '&euro; 6.00',
+   //         'game_screen' => 'bioshock_ss.jpg',
+   //         'game_dev' => 'Irrational Games',
+   //         'game_dev_url' => 'http://irrationalgames.com/tag/bioshock/',
+   //         'game_desc' => 'Fantasy 1st person shooter'
+        'BIG001_', 'Bioshock','XBox 360 2007', '&euro; 6.00', 'bioshock_ss.jpg', 'Irrational Games',
+        'http://irrationalgames.com/tag/bioshock/', 'Fantasy 1st person shooter' );
 
     $game_list[] = array(
-        'game_id' => ' BIG002 ',
-        'game_title' => 'Bioshock',
-        'game_platform' => 'PS3 2008',
-        'game_price' => '&euro; 6.00',
-        'game_screen' => 'bioshock_ss.jpg',
-        'game_dev' => 'Irrational Games',
-        'game_dev_url' => 'http://irrationalgames.com/tag/bioshock/',
-        'game_desc' => 'Fantasy 1st person shooter'
-    );
+   //     'game_id' => ' BIG002 ',
+   //     'game_title' => 'Bioshock',
+   //     'game_platform' => 'PS3 2008',
+   //     'game_price' => '&euro; 6.00',
+   //     'game_screen' => 'bioshock_ss.jpg',
+   //     'game_dev' => 'Irrational Games',
+   //     'game_dev_url' => 'http://irrationalgames.com/tag/bioshock/',
+   //     'game_desc' => 'Fantasy 1st person shooter'
+        ' BIG002 ', 'Bioshock', 'PS3 2008', '&euro; 6.00', 'bioshock_ss.jpg', 'Irrational Games',
+        'http://irrationalgames.com/tag/bioshock/', 'Fantasy 1st person shooter');
 
     $game_list[] = array(
-        'game_id' => 'BIG003',
-        'game_title' => 'Bioshock',
-        'game_platform' => 'Windows 2007',
-        'game_price' => '&euro; 6.00',
-        'game_screen' => 'bioshock_ss.jpg',
-        'game_dev' => 'Irrational Games',
-        'game_dev_url' => 'http://irrationalgames.com/tag/bioshock/',
-        'game_desc' => 'Fantasy 1st person shooter'
-    );
+   //     'game_id' => 'BIG003',
+   //     'game_title' => 'Bioshock',
+   //     'game_platform' => 'Windows 2007',
+   //     'game_price' => '&euro; 6.00',
+   //     'game_screen' => 'bioshock_ss.jpg',
+   //     'game_dev' => 'Irrational Games',
+   //     'game_dev_url' => 'http://irrationalgames.com/tag/bioshock/',
+   //     'game_desc' => 'Fantasy 1st person shooter'
+        'BIG003', 'Bioshock', 'Windows 2007', '&euro; 6.00', 'bioshock_ss.jpg', 'Irrational Games',
+        'http://irrationalgames.com/tag/bioshock/', 'Fantasy 1st person shooter');
 
     $game_list[] = array(
-        'game_id' => ' CDG003 ',
-        'game_title' => 'Chili Con Carnage',
-        'game_platform' => 'PSP 2007',
-        'game_price' => '&euro; 6.00',
-        'game_screen' => 'bioshock_ss.jpg',
-        'game_dev' => 'Deadline Games [Defunct] ',
-        'game_dev_url' => 'n/a',
-        'game_desc' => 'Comedy Action 3rd person shooter'
-    );
+   //     'game_id' => ' CDG003 ',
+   //     'game_title' => 'Chili Con Carnage',
+   //     'game_platform' => 'PSP 2007',
+   //     'game_price' => '&euro; 6.00',
+   //     'game_screen' => 'bioshock_ss.jpg',
+   //     'game_dev' => 'Deadline Games [Defunct] ',
+   //     'game_dev_url' => 'n/a',
+   //     'game_desc' => 'Comedy Action 3rd person shooter'
+        ' CDG003 ','Chili Con Carnage','PSP 2007','&euro; 6.00','bioshock_ss.jpg',
+        'Deadline Games [Defunct] ', 'n/a','Comedy Action 3rd person shooter');
 
     $game_list[] = array(
-        'game_id' => ' IE005 ',
-        'game_title' => 'Baldur\'s Gate II',
-        'game_platform' => ' Windows ',
-        'game_price' => '&euro; 6.00',
-        'game_screen' => 'baldursgate_ss.png',
-        'game_dev' => 'Interplay Ent Corp ',
-        'game_dev_url' => ' http://www.interplay.com/ ',
-        'game_desc' => ' Fantasy CRPG '
-    );
+   //     'game_id' => ' IE005 ',
+   //     'game_title' => 'Baldur\'s Gate II',
+   //     'game_platform' => ' Windows ',
+   //     'game_price' => '&euro; 6.00',
+   //     'game_screen' => 'baldursgate_ss.png',
+   //     'game_dev' => 'Interplay Ent Corp ',
+   //     'game_dev_url' => ' http://www.interplay.com/ ',
+   //     'game_desc' => ' Fantasy CRPG '
+        ' IE005 ', 'Baldur\'s Gate II',' Windows ','&euro; 6.00','baldursgate_ss.png',
+        'Interplay Ent Corp ', ' http://www.interplay.com/ ',' Fantasy CRPG ');
 
     $game_list[] = array(
-        'game_id' => ' SEA004 ',
-        'game_title' => ' The Sims 2 ',
-        'game_platform' => ' Nintendo DS 2005 ',
-        'game_price' => '&euro; 6.00',
-        'game_screen' => 'sims2_ss.jpg',
-        'game_dev' => ' Electronic Arts ',
-        'game_dev_url' => ' http://www.ea.com/ ',
-        'game_desc' => ' Life Simulation '
-    );
+   //     'game_id' => ' SEA004 ',
+   //     'game_title' => ' The Sims 2 ',
+   //     'game_platform' => ' Nintendo DS 2005 ',
+  //      'game_price' => '&euro; 6.00',
+  //      'game_screen' => 'sims2_ss.jpg',
+  //      'game_dev' => ' Electronic Arts ',
+  //      'game_dev_url' => ' http://www.ea.com/ ',
+   //     'game_desc' => ' Life Simulation '
+        ' SEA004 ', ' The Sims 2 ', ' Nintendo DS 2005 ','&euro; 6.00','sims2_ss.jpg',
+        ' Electronic Arts ',' http://www.ea.com/ ',' Life Simulation ');
 
     $game_list[] = array(
-        'game_id' => ' SEA007 ',
-        'game_title' => ' The Sims 2 ',
-        'game_platform' => ' GameCube 2005 ',
-        'game_price' => '&euro; 6.00',
-        'game_screen' => 'sims2_ss.jpg',
-        'game_dev' => ' Electronic Arts ',
-        'game_dev_url' => ' http://www.ea.com/ ',
-        'game_desc' => ' Life Simulation '
-    );
+   //     'game_id' => ' SEA007 ',
+   //     'game_title' => ' The Sims 2 ',
+   //     'game_platform' => ' GameCube 2005 ',
+   //     'game_price' => '&euro; 6.00',
+   //     'game_screen' => 'sims2_ss.jpg',
+   //     'game_dev' => ' Electronic Arts ',
+   //     'game_dev_url' => ' http://www.ea.com/ ',
+   //     'game_desc' => ' Life Simulation '
+        ' SEA007 ',' The Sims 2 ',' GameCube 2005 ','&euro; 6.00', 'sims2_ss.jpg',
+        ' Electronic Arts ', ' http://www.ea.com/ ',' Life Simulation ');
 
     $game_list[] = array(
-        'game_id' => ' PHMR02 ',
-        'game_title' => ' Prince of Persia ',
-        'game_platform' => ' Sega Master System 1992 ',
-        'game_price' => '&euro; 26.00',
-        'game_screen' => 'prince_persia_ss.png',
-        'game_dev' => ' Broderbund Software [defunct]  ',
-        'game_dev_url' => ' n/a ',
-        'game_desc' => ' Fantasy '
-    );
+   //     'game_id' => ' PHMR02 ',
+   //     'game_title' => ' Prince of Persia ',
+   //     'game_platform' => ' Sega Master System 1992 ',
+   //     'game_price' => '&euro; 26.00',
+   //     'game_screen' => 'prince_persia_ss.png',
+   //     'game_dev' => ' Broderbund Software [defunct]  ',
+   //     'game_dev_url' => ' n/a ',
+   //     'game_desc' => ' Fantasy '
+        ' PHMR02 ',' Prince of Persia ',' Sega Master System 1992 ','&euro; 26.00',
+        'prince_persia_ss.png', ' Broderbund Software [defunct]  ', ' n/a ',' Fantasy ');
 
     $game_list[] = array(
-        'game_id' => ' PHMR75 ',
-        'game_title' => ' Prince of Persia ',
-        'game_platform' => ' Gameboy Color 1999 ',
-        'game_price' => '&euro; 26.00',
-        'game_screen' => 'prince_persia_ss.png',
-        'game_dev' => ' Broderbund Software [defunct]  ',
-        'game_dev_url' => ' n/a ',
-        'game_desc' => ' Fantasy '
-    );
+   //     'game_id' => ' PHMR75 ',
+   //     'game_title' => ' Prince of Persia ',
+   //     'game_platform' => ' Gameboy Color 1999 ',
+   //     'game_price' => '&euro; 26.00',
+   //     'game_screen' => 'prince_persia_ss.png',
+   //     'game_dev' => ' Broderbund Software [defunct]  ',
+   //     'game_dev_url' => ' n/a ',
+   //     'game_desc' => ' Fantasy '
+        ' PHMR75 ', ' Prince of Persia ',' Gameboy Color 1999 ','&euro; 26.00',
+        'prince_persia_ss.png', ' Broderbund Software [defunct]  ', ' n/a ',' Fantasy ');
 
     $game_list[] = array(
-        'game_id' => ' PHMR08 ',
-        'game_title' => ' Prince of Persia ',
-        'game_platform' => ' Amstrad PCP 1990 ',
-        'game_price' => '&euro; 50.00',
-        'game_screen' => 'prince_persia_ss.png',
-        'game_dev' => ' Broderbund Software [defunct]  ',
-        'game_dev_url' => ' n/a ',
-        'game_desc' => ' Fantasy '
-    );
+   //     'game_id' => ' PHMR08 ',
+   //     'game_title' => ' Prince of Persia ',
+   //     'game_platform' => ' Amstrad PCP 1990 ',
+   //     'game_price' => '&euro; 50.00',
+   //     'game_screen' => 'prince_persia_ss.png',
+   //     'game_dev' => ' Broderbund Software [defunct]  ',
+   //     'game_dev_url' => ' n/a ',
+   //     'game_desc' => ' Fantasy '
+        ' PHMR08 ', ' Prince of Persia ',' Amstrad PCP 1990 ','&euro; 26.00',
+        'prince_persia_ss.png', ' Broderbund Software [defunct]  ', ' n/a ',' Fantasy ');
 
 
-    $content = '';
+    require_once __DIR__ . '/../templates/shop.php';
 
-    while ( $game = current($game_list) ) {
-        ob_start();
-    ?>
-<tr>
-    <td> <?= $game['game_id'] ?> </td>
-    <td> <?= $game['game_title'] ?> </td>
-    <td> <?= $game['game_platform'] ?> </td>
-    <td> <?= $game['game_price'] ?> </td>
-    <td><img src="../public/images/<?= $game['game_screen'] ?>" alt="<?= $game['game_title'] ?> screen" width="100%" height="auto"></td>
-    <td><a href="<?= $game['game_dev_url'] ?>" target="blank"><?= $game['game_dev'] ?></a></td>
-    <td> <?= $game['game_desc'] ?> </td>
-</tr>
-
-<?php
-        $table_row = ob_get_contents();
-        ob_end_clean();
-        $content .= $table_row;
-        next($game_list);
-    }
-
-    echo $content;
 }
--- stage_02_php/templates/shop.php
+++ stage_02_php/templates/shop.php
@@ -40,7 +40,34 @@ require_once __DIR__ . '/../templates/nav.inc.php';
 
     <?php
     //require_once ;
-    shopListingAction();
+    //shopListingAction();
+
+
+    $content = '';
+
+    while ( $game = current($game_list) ) {
+        ob_start();
+        ?>
+        <tr>
+            <td> <?= $game->getGameId() ?> </td>
+            <td> <?= $game->getGameTitle() ?> </td>
+            <td> <?= $game->getGamePlatform() ?> </td>
+            <td> <?= $game->getGamePrice() ?> </td>
+            <td><img src="../public/images/<?= $game->getGameScreen() ?>" alt="<?= $game->getGameTitle() ?> screen" width="100%" height="auto"></td>
+            <td><a href="<?= $game->getGameDevUrl() ?>" target="blank"><?= $game->getGameDev() ?></a></td>
+            <td> <?= $game->getGameDesc() ?> </td>
+        </tr>
+
+        <?php
+        $table_row = ob_get_contents();
+        ob_end_clean();
+        $content .= $table_row;
+        next($game_list);
+    }
+
+    echo $content;
+
+
     ?>
 
 </table>
