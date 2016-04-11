bf18432e8d67cbecd52f75e0371b04b444b36ef1
Fiona Delaney <B00092671@student.itb.ie>
2016-04-11 21:50:25 +0100

    content tidy on shop page

--- stage_02_php/src/mainController.php
+++ stage_02_php/src/mainController.php
@@ -43,14 +43,13 @@ function shopAction()
     $pageTitle = 'Shop';
     $shopLinkStyle = 'current_page';
     require_once __DIR__ . '/../templates/shop.php';
-    require_once __DIR__ . '/../src/game.php';
 
 }
 
  function shopListingAction()
  {
     $game_list = [];
-    $game_list[] = new Game('BIG001_', 'Bioshock','XBox 360 2007', '&euro; 6.00',
+    $game_list[] = new Game('BIG001', 'Bioshock','XBox 360 2007', '&euro; 6.00',
         'bioshock_ss.jpg', 'Irrational Games', 'http://irrationalgames.com/tag/bioshock/',
         'Fantasy 1st person shooter');
 
@@ -60,8 +59,8 @@ function shopAction()
     $game_list[] = new Game('BIG003', 'Bioshock', 'Windows 2007', '&euro; 6.00', 'bioshock_ss.jpg',
         'Irrational Games', 'http://irrationalgames.com/tag/bioshock/', 'Fantasy 1st person shooter');
 
-    $game_list[] = new Game(' CDG003 ','Chili Con Carnage','PSP 2007','&euro; 6.00','bioshock_ss.jpg',
-        'Deadline Games [Defunct] ', 'n/a','Comedy Action 3rd person shooter');
+    $game_list[] = new Game(' CDG003 ','Chili Con Carnage','PSP 2007','&euro; 6.00','chili.jpg',
+        'Deadline Games [Defunct] ', 'http://www.mobygames.com/company/deadline-games-as/','Comedy Action 3rd person shooter');
 
     $game_list[] = new Game(' IE005 ', 'Baldur\'s Gate II',' Windows ','&euro; 6.00','baldursgate_ss.png',
         'Interplay Ent Corp ', ' http://www.interplay.com/ ',' Fantasy CRPG ');
@@ -73,13 +72,16 @@ function shopAction()
         ' Electronic Arts ', ' http://www.ea.com/ ',' Life Simulation ');
 
     $game_list[] = new Game(' PHMR02 ',' Prince of Persia ',' Sega Master System 1992 ','&euro; 26.00',
-        'prince_persia_ss.png', ' Broderbund Software [defunct]  ', ' n/a ',' Fantasy ');
+        'prince_persia_ss.png',' Broderbund Software [defunct]  ',
+        ' http://www.mobygames.com/company/brderbund-software-inc/ ',' Fantasy ');
 
     $game_list[] = new Game(' PHMR75 ', ' Prince of Persia ',' Gameboy Color 1999 ','&euro; 26.00',
-        'prince_persia_ss.png', ' Broderbund Software [defunct]  ', ' n/a ',' Fantasy ');
+        'prince_persia_ss.png', ' Broderbund Software [defunct]  ',
+        ' http://www.mobygames.com/company/brderbund-software-inc/',' Fantasy ');
 
     $game_list[] = new Game(' PHMR08 ', ' Prince of Persia ',' Amstrad PCP 1990 ','&euro; 26.00',
-        'prince_persia_ss.png', ' Broderbund Software [defunct]  ', ' n/a ',' Fantasy ');
+        'prince_persia_ss.png', ' Broderbund Software [defunct]  ',
+        ' http://www.mobygames.com/company/brderbund-software-inc/ ',' Fantasy ');
 
 
 return $game_list;
